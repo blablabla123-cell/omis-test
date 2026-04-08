59a6a5588ba4e68ea4b1baad4e7e6d88a1accf40
blablabla123-cell <v.ardjanidis@yandex.ru>
2026-04-08 19:54:45 +0300

    latest

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 export default () => {
-  console.log(`${process.env.DATABASE_URL}`);
   const port = process.env.PORT || '3000';
   const postgresPort = process.env.POSTGRES_PORT || '5432';
 
